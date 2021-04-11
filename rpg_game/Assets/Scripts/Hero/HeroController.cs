ba1011a88992bd75f18eef1c2a99ab26574a0d06
Evan <etredal@gmail.com>
2021-04-11 16:29:35 -0500

    Gabe Idle Code
    Added Bool Param.  Removed Exit Only for transitions.  Added code section for anim.

--- rpg_game/Assets/Scripts/Hero/HeroController.cs
+++ rpg_game/Assets/Scripts/Hero/HeroController.cs
@@ -7,10 +7,12 @@ public class HeroController : MonoBehaviour
     private SpriteRenderer sr;
     float moveSpeed = 0.8f;
     private Rigidbody2D body;
+    private Animator animator;
 
     void Awake() {
         sr = GetComponent<SpriteRenderer>();
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,10 +22,18 @@ public class HeroController : MonoBehaviour
 
         body.velocity = new Vector2(horizontalInput * moveSpeed, verticalInput * moveSpeed);
 
+        // Sprite Renderer Logic
         if (horizontalInput > 0) {
             sr.flipX = false;
         } else if (horizontalInput < 0) {
             sr.flipX = true;
+        }
+
+        // Animation Logic
+        if (Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput) != 0) {
+            animator.SetBool("isMoving", true);
+        } else {
+            animator.SetBool("isMoving", false);
         }
     }
 }
