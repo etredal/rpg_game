13fc7eabd0162fe4d9c2edf12c1b038ee2b3cfa1
Evan <etredal@gmail.com>
2021-04-21 22:16:31 -0500

    Slime Patrol
    RB for Slime, checkpoints objects, script for AI Checkpoint

---
+++ rpg_game/Assets/Scripts/Mobs/MobAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobAI : MonoBehaviour
+{
+    public float movementSpeed = 0f;
+    public GameObject checkpoints;
+    private List<Vector2> checkpointLocations;
+    private int currentCheckpoint = 0;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        checkpointLocations = new List<Vector2>();
+        rb = GetComponent<Rigidbody2D>();
+
+        for (int n = 0; n < checkpoints.transform.childCount; n++) {
+            checkpointLocations.Add(checkpoints.transform.GetChild(n).position);
+        }
+
+        Destroy(checkpoints);
+    }
+
+    void Update()
+    {
+        Vector2 moveVelocity = checkpointLocations[currentCheckpoint] - (Vector2) transform.position;
+        rb.velocity = moveVelocity.normalized * movementSpeed;
+
+        // Reached checkpoint
+        if (Vector2.Distance(transform.position, checkpointLocations[currentCheckpoint]) < 0.1f)
+        {
+            currentCheckpoint = (currentCheckpoint + 1) % checkpointLocations.Count;
+        }
+    }
+}
