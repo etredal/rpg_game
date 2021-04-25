018edd1e32c256dbf770780e08ebb37411e2afee
Evan <etredal@gmail.com>
2021-04-25 13:49:13 -0500

    Added Scripts
    Gamecontroller, Basic Scene Management

---
+++ rpg_game/Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Initialize")
+        {
+            SceneManager.LoadScene("Forest-1");
+        }
+    }
+}
