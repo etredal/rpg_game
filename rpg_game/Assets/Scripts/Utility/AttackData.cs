0439d2dd89553a6cf7cea6fdd9b63d226b0c97a8
Evan <etredal@gmail.com>
2021-06-24 16:14:12 -0500

    Enemy Data Additions
    GameController Tags, Script changes, anim update for slime

---
+++ rpg_game/Assets/Scripts/Utility/AttackData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class AttackData
+{
+    public string AttackName;
+    public float AttackDamage;
+    public float AttackChance;
+}
---
+++ rpg_game/Assets/Scripts/Utility/EnemyData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class EnemyData
+{
+    public string EnemyType;
+    public string EnemyName;
+    public float HP;
+    public AttackData[] Attacks;
+}
